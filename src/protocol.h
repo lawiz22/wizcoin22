6e4b46f947865360a022895fe40e24d775995d44
wizcoin <wizbtc@gmail.com>
2014-04-07 21:46:24 -0400

    Dans la vie comme au hockey...

--- src/protocol.h
+++ src/protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 22422 : 55884;
+    return testnet ? 33223 : 55884;
 }
 
 
