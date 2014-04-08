9520e1b0fc753ab0c690b5c7028f49ea67a549f8
wizcoin <wizbtc@gmail.com>
2014-04-07 21:20:42 -0400

    Faudrait que un jour cela cesse

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -39,7 +39,7 @@ static boost::thread_group* rpc_worker_group = NULL;
 
 static inline unsigned short GetDefaultRPCPort()
 {
-    return GetBoolArg("-testnet", false) ? 45883 : 55883;
+    return GetBoolArg("-testnet", false) ? 45884 : 55883;
 }
 
 Object JSONRPCError(int code, const string& message)
