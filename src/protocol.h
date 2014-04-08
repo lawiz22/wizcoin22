827ba0a4432354229287e6a1580afede90602893
wizcoin <wizbtc@gmail.com>
2014-04-07 22:39:50 -0400

    Bon dernier coup

--- src/protocol.h
+++ src/protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 12465 : 55884;
+    return testnet ? 34666 : 55883;
 }
 
 
